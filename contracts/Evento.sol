9f236ee7c1bd035a181c5e1d90e0e4ed66262d96
Paulo Vitor <paulonovaes@id.uff.br>
2024-06-18 19:43:39 -0300

    atualizando nome

--- contracts/Evento.sol
+++ contracts/Evento.sol
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract CertificateContract {
-    struct Certificate {
+contract CertificadoContract {
+    struct Certificado {
         string nomeEvento;
         string horasComplementares; 
         string nomeParticipante;
     }
 
-    mapping(address => Certificate[]) public certificates;
+    mapping(address => Certificado[]) public certificados;
 
     event CertificateIssued(address indexed participant, string nomeEvento, string horasComplementares, string nomeParticipante);
 
     function gerarCertificado(address _participant, string memory _nomeEvento, string memory _horasComplementares, string memory _nomeParticipante) public {
-        certificates[_participant].push(Certificate({
+        certificados[_participant].push(Certificado({
             nomeEvento: _nomeEvento,
             horasComplementares: _horasComplementares,
             nomeParticipante: _nomeParticipante
@@ -23,12 +23,12 @@ contract CertificateContract {
     }
 
     function getCertificado(address _participant, uint index) public view returns (string memory, string memory, string memory) {
-        require(index < certificates[_participant].length, "Certificado nao encontrado");
-        Certificate memory cert = certificates[_participant][index];
+        require(index < certificados[_participant].length, "Certificado nao encontrado");
+        Certificado memory cert = certificados[_participant][index];
         return (cert.nomeEvento, cert.horasComplementares, cert.nomeParticipante);
     }
 
-    function getNumberOfCertificates(address _participant) public view returns (uint) {
-        return certificates[_participant].length;
+    function getNumberOfCertificados(address _participant) public view returns (uint) {
+        return certificados[_participant].length;
     }
 }
