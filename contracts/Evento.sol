aebb5a36075b9e03082e7a408986fded26b7d507
Paulo Vitor <paulonovaes@id.uff.br>
2024-06-18 19:30:13 -0300

    permitindo varios certificados por endereco

--- contracts/Evento.sol
+++ contracts/Evento.sol
@@ -4,34 +4,31 @@ pragma solidity ^0.8.0;
 contract CertificateContract {
     struct Certificate {
         string nomeEvento;
-        uint horasComplementares;
+        string horasComplementares; 
         string nomeParticipante;
-        bool exists;
     }
 
-    mapping(address => Certificate) public certificates;
+    mapping(address => Certificate[]) public certificates;
 
-    event CertificateIssued(address indexed participant, string nomeEvento, uint horasComplementares, string nomeParticipante);
+    event CertificateIssued(address indexed participant, string nomeEvento, string horasComplementares, string nomeParticipante);
 
-    function gerarCertificado(address _participant, string memory _nomeEvento, uint _horasComplementares, string memory _nomeParticipante) public {
-        require(!certificates[_participant].exists, "Certificado ja existe para esse participante");
-
-        certificates[_participant] = Certificate({
+    function gerarCertificado(address _participant, string memory _nomeEvento, string memory _horasComplementares, string memory _nomeParticipante) public {
+        certificates[_participant].push(Certificate({
             nomeEvento: _nomeEvento,
             horasComplementares: _horasComplementares,
-            nomeParticipante: _nomeParticipante,
-            exists: true
-        });
+            nomeParticipante: _nomeParticipante
+        }));
 
         emit CertificateIssued(_participant, _nomeEvento, _horasComplementares, _nomeParticipante);
     }
 
-    function validarCertificado(address _participant) public view returns (bool) {
-        return certificates[_participant].exists;
+    function getCertificado(address _participant, uint index) public view returns (string memory, string memory, string memory) {
+        require(index < certificates[_participant].length, "Certificado nao encontrado");
+        Certificate memory cert = certificates[_participant][index];
+        return (cert.nomeEvento, cert.horasComplementares, cert.nomeParticipante);
     }
 
-    function getCertificado(address _participant) public view returns (string memory, uint, string memory, bool) {
-        Certificate memory cert = certificates[_participant];
-        return (cert.nomeEvento, cert.horasComplementares, cert.nomeParticipante, cert.exists);
+    function getNumberOfCertificates(address _participant) public view returns (uint) {
+        return certificates[_participant].length;
     }
 }
